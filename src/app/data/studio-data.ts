2a451981c5fba4777fd9e13086fce9f226a0e7e1
Demeter Bálint <demibalint@gmail.com>
2026-02-09 17:29:02 +0100

    studio - model and data created

---
+++ src/app/data/studio-data.ts
@@ -0,0 +1,63 @@
+import {StudioModel} from '../models/studio-model';
+
+export const studioData: StudioModel = {
+  brand: {
+    hu: '4 Plusz Építész Stúdió',
+    en: '4 Plusz Architect Studio'
+  },
+  part1Title: {
+    hu: 'Alapítva 1993-ban',
+    en: 'Since 1993'
+  },
+  part1Text: {
+    hu: ['Irodánkat 1993-ban alapítottuk azzal a szándékkal, hogy saját gondolkodásmódunk, metodikánk szerint tervezhessünk, és alkotó módon vehessünk részt egyedi, magas nívójú épületek, terek, tárgyak létrehozásában. Gondolkodásmódunkat meghatározza, hogy épületeink pozitívan alakítsák közvetlen, vagy akár tágabb környezetüket. Helyszíni, településképi, városszöveti, vagy természeti adottságokra ugyanúgy reagáljanak, mint a befogadó, felhasználó környezet kultúrális, vagy szociális adottságaira.',
+      'Ugyanakkor ez a fajta idomulás a környezetükből minőségükben kiemelkedő, iránymutató és invenciózus alkotásokkal valósuljon meg. A környezettudatosság ma már nem luxus, hanem túlélésünk lehetősége. Ezért épületeink tervezésénél a felhasználható, visszaforgatható, megtakarítható energiák feltérképezése, ésszerű hasznosítása alapvető jelentőségű. Műemlékekkel kapcsolatos munkáinknál célunk a megmaradt értékeke megőrzésén és a múlt iránti alázaton túl építészeti eszközökkel olyan többletet adni, amely a meglévő örökség minőségének és szellemiségének továbbvitelében és mai, korszerű megfogalmazásában jelenik meg.'],
+    en: ['We founded our office in 1993 with the intention of designing according to our own way of thinking and methodology, and participating creatively in the creation of unique, high-quality buildings, spaces, and objects. Our mindset is defined by the desire for our buildings to have a positive impact on their immediate and wider surroundings. They should respond to the characteristics of the location, the townscape, the urban fabric, and the natural environment in the same way as they respond to the cultural and social characteristics of the environment that hosts and uses them.',
+      'At the same time, this type of adaptation should be achieved through works that stand out from their surroundings in terms of quality, guidance, and inventiveness. Environmental awareness is no longer a luxury, but a matter of survival. Therefore, when designing our buildings, it is essential to identify and make rational use of usable, recyclable, and renewable energy sources. In our work with historic buildings, our goal is to preserve their remaining value and, beyond our respect for the past, to use architectural tools to add something extra that reflects the quality and spirit of the existing heritage in a modern, contemporary form.']
+  },
+  part2Title: {
+    hu: 'Díjak és elismerések',
+    en: 'Awards and recognitions'
+  },
+  part2Text: {
+    hu: ['Pro Architectura díj – Egek Királynéja Római Katolikus Főplébánia',
+      'Építőipari nívódíj – középület / egyházi létesítmény kategória – Ráday-Ház',
+      'Építőipari nívódíj – középület /egyházi létesítmény kategória - Egek Királynéja Római Katolikus Főplébánia',
+      'Budapesti Építész Kamara Nívódíj jelölt - Egek Királynéja Római Katolikus Főplébánia',
+      'Archdaily – Building of the Year 2016 – jelölt - Egek Királynéja Római Katolikus Főplébánia',
+      'European Copper in Architecture Award 15 – pályázat, kiállítás Bruxelles - Polgármesteri Hivatal bővítése- étterem és irodaépület',
+      'Év homlokzata III. hely – Bp. Kiscelli úti társasház',
+      'Év tetője II. hely – Szarvaskút vendégház',
+      '„Jövőt építünk!” pályázat díj – Fasori Református Kollégium Csipkebokor óvodája',
+      'BIG ZAVOD kiállítás és verseny meghívás – Ljubljana'],
+    en: ['Pro Architectura award – Parish Church of the Celestial Queen',
+      'Construction Quality-award – public building / religious facility category – Ráday-House',
+      'Construction Quality-award – public building / religious facility category - Parish Church of the Celestial Queen',
+      'Budapest Architect Chamber Quality-award nomination - Parish Church of the Celestial Queen',
+      'Archdaily – Building of the Year 2016 – jelölt - Parish Church of the Celestial Queen',
+      'European Copper in Architecture Award 15 – competition, exhibition Bruxelles - Keresztúr Restaurant and Office Building',
+      'Facade of the year III. place – Bp. Kiscelli road apartment building',
+      'Roof of the year II. place – Szarvaskút guest house',
+      '„Jövőt építünk!” competition award – Fasori Reformed College Csipkebokor Kindergarden',
+      'BIG ZAVOD exhibition and competition invitation – Ljubljana']
+  },
+  part3Name: {
+    hu: 'Berzsák Zoltán',
+    en: 'Zoltán Berzsák'
+  },
+  part3NameText: {
+    hu: ['Pro-Architectura és Nívódíjas építész vezető tervező, ügyvezető',
+      'A stúdió vezetője és alapítója'],
+    en: ['Architect awarded by Pro-Architectura and Quality-Award, chief architect, executive manager',
+    'Studio manager and founder']
+  },
+  part3Title: {
+    hu: 'Jelenlegi és egykori munkatársak',
+    en: 'Current and former employees'
+  },
+  part3TitleText: {
+    hu: 'Balogh Eszter, Berzsák Barna, Csintalan Csongor, Eperjesi Blanka, Egri Balázs, Farkas Eszter, Göös Petronella, Juhász Andrea, Hortobágyi Éva, Kerényi Szilvia, Kiss Zoltán, Konyha Katalin, Koós Mariann, Mátis Barna, Monori-Szabó Edit, Mravik Csaba, Sonkoly András, Szájer Andrea, Szvák Dorottya, Pálinkás Yvett, Rácz Noémi, Vékony Péter, Zacher Eszter',
+    en: 'Eszter Balogh, Barna Berzsák, Csongor Csintalan, Blanka Eperjesi, Balázs Egri, Eszter Farkas, Petronella Göös, Andrea Juhász, Éva Hortobágyi, Szilvia Kerényi, Zoltán Kiss, Katalin Konyha, Mariann Koós, Barna Mátis, Edit Monori-Szabó, Csaba Mravik, András Sonkoly, Andrea Szájer, Dorottya Szvák, Yvett Pálinkás, Noémi Rácz, Péter Vékony, Eszter Zacher'
+  },
+  part3ImageURL: 'berzsi-img'
+}
---
+++ src/app/models/studio-model.ts
@@ -0,0 +1,39 @@
+export interface StudioModel {
+  brand: {
+    hu: string,
+    en: string
+  },
+  part1Title: {
+    hu: string,
+    en: string
+  },
+  part1Text: {
+    hu: string[],
+    en: string[]
+  },
+  part2Title: {
+    hu: string,
+    en: string
+  },
+  part2Text: {
+    hu: string[],
+    en: string[]
+  },
+  part3Name: {
+    hu: string,
+    en: string
+  },
+  part3NameText: {
+    hu: string[],
+    en: string[]
+  },
+  part3Title: {
+    hu: string,
+    en: string
+  },
+  part3TitleText: {
+    hu: string,
+    en: string
+  },
+  part3ImageURL: string
+}
