0830487509289a529977d9940450b8cffd6b6380
Demeter Bálint <demibalint@gmail.com>
2026-02-09 19:40:42 +0100

    language stays the same on refresh

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -8,12 +8,18 @@ export type Language = 'en' | 'hu';
 })
 export class LanguageService {
 
-  constructor() { }
-
   private languageSubject = new BehaviorSubject<Language>('hu');
   language$ = this.languageSubject.asObservable();
 
+  constructor() {
+    const saved = localStorage.getItem('lang') as Language | null;
+    if (saved === 'en' || saved === 'hu') {
+      this.languageSubject.next(saved)
+    }
+  }
+
   setLanguage(lang: Language) {
+    localStorage.setItem('lang', lang);
     this.languageSubject.next(lang);
   }
 
