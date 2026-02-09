624334970dbb35f81abb6c251c62be5f79348cc4
Demeter Bálint <demibalint@gmail.com>
2026-02-09 20:06:11 +0100

    contact - data and model (title and data together)

---
+++ src/app/data/contact-data.ts
@@ -0,0 +1,28 @@
+import {ContactModel} from '../models/contact-model';
+
+export const CONTACT_DATA: ContactModel = {
+  addressTitle: {
+    hu: 'Cím',
+    en: 'Address'
+  },
+  address: {
+    hu: '1111 Budapest, Bartók Béla út 18',
+    en: '1111 Budapest, Bartók Béla street 18'
+  },
+  phoneTitle: {
+    hu: 'Telefonszám',
+    en: 'Phone',
+  },
+  phone: {
+    hu: '+36 20 982 6352',
+    en: '+36 20 982 6352'
+  },
+  email: {
+    hu: '4pluszkft@gmail.com',
+    en: '4pluszkft@gmail.com'
+  },
+  followTitle: {
+    hu: 'Követés',
+    en: 'Follow'
+  }
+}
---
+++ src/app/models/contact-model.ts
@@ -0,0 +1,26 @@
+export interface ContactModel {
+  addressTitle: {
+    hu: string,
+    en: string
+  },
+  address: {
+    hu: string,
+    en: string
+  },
+  phoneTitle: {
+    hu: string,
+    en: string
+  },
+  phone: {
+    hu: string,
+    en: string
+  },
+  email: {
+    hu: string,
+    en: string
+  },
+  followTitle: {
+    hu: string,
+    en: string
+  }
+}
