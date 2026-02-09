dcce73fb342331a9d74fe00ca182048f597d8012
Demeter Bálint <demibalint@gmail.com>
2026-02-09 19:43:18 +0100

    studio - done; award links not set yet

--- src/app/components/studio/studio.component.ts
+++ src/app/components/studio/studio.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Language, LanguageService} from '../../services/language.service';
+import {StudioModel} from '../../models/studio-model';
+import {STUDIO_DATA} from '../../data/studio-data';
+import {NgForOf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-studio',
-  imports: [],
+  imports: [
+    NgForOf,
+    RouterLink
+  ],
   templateUrl: './studio.component.html',
   styleUrl: './studio.component.css'
 })
-export class StudioComponent {
+export class StudioComponent implements OnInit {
+  currentLang: Language = 'hu';
+  studio: StudioModel = STUDIO_DATA;
+
+  constructor(public languageService: LanguageService) {}
+
+  ngOnInit(): void {
+      this.languageService.language$.subscribe(lang => {
+        this.currentLang = lang;
+      })
+  }
 
 }
