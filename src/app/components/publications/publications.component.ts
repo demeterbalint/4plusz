890f82ef5a03025939bd0a86f491479e0414d44e
Demeter Bálint <demibalint@gmail.com>
2026-02-10 16:06:09 +0100

    publications - done

--- src/app/components/publications/publications.component.ts
+++ src/app/components/publications/publications.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Language, LanguageService} from '../../services/language.service';
+import {PublicationModel} from '../../models/publication--model';
+import {PUBLICATIONS_DATA} from '../../data/publications-data';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-publications',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './publications.component.html',
   styleUrl: './publications.component.css'
 })
-export class PublicationsComponent {
+export class PublicationsComponent implements OnInit {
+
+  currentLang: Language = 'hu';
+  publicationsData: PublicationModel[] = PUBLICATIONS_DATA;
+  title: any = {
+    hu: 'Sajtó megjelenések',
+    en: 'Press appearances'
+  }
+
+  subtitle: any = {
+    hu: 'Az alábbi felsorolásban építészeti szaklapok kritikái olvashatóak munkáinkról.',
+    en: 'The list below includes reviews of our work from architectural journals.'
+  }
+
+  constructor(private languageService: LanguageService) {
+  }
+
+  ngOnInit(): void {
+    this.languageService.language$.subscribe(lang => {
+      this.currentLang = lang;
+    })
+  }
 
 }
