b3ab414a1329127feb1d403493c5edea764ccbe7
Demeter Bálint <demibalint@gmail.com>
2026-02-12 16:04:55 +0100

    project model updated with gridshort which appears on grid hover

--- src/app/models/project-model.ts
+++ src/app/models/project-model.ts
@@ -1,20 +1,24 @@
 export interface ProjectModel {
-  slug: string;
+  slug: string,
   name: {
-    hu: string;
-    en: string;
-  };
+    hu: string,
+    en: string
+  },
+  gridShort: {
+    hu: string,
+    en: string
+  },
   shortDescription: {
-    hu: string;
-    en: string;
-  };
+    hu: string,
+    en: string
+  },
   longDescription: {
-    hu: string;
-    en: string;
-  };
+    hu: string[],
+    en: string[]
+  },
   additionalInfo: {
-    hu: string;
-    en: string;
-  };
-  images: string[];
+    hu: string[],
+    en: string[]
+  },
+  images: string[]
 }
