5da68e9ccc9a71b2dab1317ed4cdbd21c4b4b9db
Demeter Bálint <demibalint@gmail.com>
2026-02-16 23:24:46 +0100

    project-type dropdown menu and routing set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/route
 import {Language, LanguageService} from './services/language.service';
 import {NavbarFooterTextModel} from './models/navbar-footer-text-model';
 import {NAVBAR_FOOTER_TEXT} from './data/navbar-footer-text-data';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,6 +15,24 @@ export class AppComponent implements OnInit {
   title = 'negyplusz';
 
   currentLang: Language = 'hu';
+  projectTypes = {
+    hu: [
+      { name: 'Középületek', route: 'public-buildings' },
+      { name: 'Szakrális épületek', route: 'religious-buildings' },
+      { name: 'Lakóépületek', route: 'residential-buildings' },
+      { name: 'Műemlékek', route: 'historical-buildings' },
+      { name: 'Tervek', route: 'conceptual-buildings' },
+      { name: 'Referenciák', route: 'reference-works' }
+    ],
+    en: [
+      { name: 'Public buildings', route: 'public-buildings' },
+      { name: 'Religious buildings', route: 'religious-buildings' },
+      { name: 'Residential buildings', route: 'residential-buildings' },
+      { name: 'Historical buildings', route: 'historical-buildings' },
+      { name: 'Conceptual designs', route: 'conceptual-buildings' },
+      { name: 'Reference works', route: 'reference-works' }
+    ]
+  }
 
   constructor(private router: Router,
               private languageService: LanguageService) {
@@ -39,12 +58,4 @@ export class AppComponent implements OnInit {
     const url = 'https://www.google.com/maps/dir//4+Plusz+Kft.,+Budapest,+Bart%C3%B3k+B%C3%A9la+%C3%BAt+18,+1111/@47.4814979,19.0527283,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4741ddb65e482c5d:0x93de8965a16efc54!2m2!1d19.0527283!2d47.4814979?entry=ttu&g_ep=EgoyMDI2MDIxMS4wIKXMDSoASAFQAw%3D%3D'
     window.open(url, '_blank');
   }
-
-  /*protected goToHome() {
-    if (this.router.isActive('/', true)) {
-      window.scrollTo({top: 0, left: 0, behavior: "smooth"});
-    } else {
-      this.router.navigate(['/']);
-    }
-  }*/
 }
