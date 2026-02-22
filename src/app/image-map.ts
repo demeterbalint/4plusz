fa9bd20232ad9e7453bcb0e957e3e432b6d9674c
Demeter Bálint <demibalint@gmail.com>
2026-02-23 00:10:02 +0100

    veszprem - main image change; ujpest - gallery order; gallery arrows aligned with close button

--- src/app/image-map.ts
+++ src/app/image-map.ts
@@ -134,10 +134,10 @@ export const imageMap: Record<string, string[]> = {
 ],
   'family-house-5': [
     "assets/buildings/family-house-5/veszprem-01.jpg",
-    "assets/buildings/family-house-5/veszprem-02-main.jpg",
+    "assets/buildings/family-house-5/veszprem-02.jpg",
     "assets/buildings/family-house-5/veszprem-03.jpg",
     "assets/buildings/family-house-5/veszprem-04.jpg",
-    "assets/buildings/family-house-5/veszprem-05.webp",
+    "assets/buildings/family-house-5/veszprem-05-main.webp",
     "assets/buildings/family-house-5/veszprem-grid.jpg",
     "assets/buildings/family-house-5/veszprem-residential.webp"
 ],
